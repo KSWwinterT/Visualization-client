63abdeefbdd0da13c305f2877206a82fa76b0b2b
JuEun Kim <kje0405@naver.com>
2024-02-17 20:05:21 -0500

    pie chart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MainPage from "./MainPage";
+import PieChart from "./PieChart";
 
 function App() {
   return (
       <BrowserRouter>
     <div className="App">
       <header className="App-header">
-          <Routes>
-              <Route path="/" element={<MainPage/>}/>
-          </Routes>
       </header>
+        <Routes>
+            <Route path="/" element={<MainPage/>}/>
+            <Route path="/pie" element={<PieChart/>}/>
+        </Routes>
     </div>
       </BrowserRouter>
   );
--- src/MainPage.js
+++ src/MainPage.js
@@ -3,6 +3,7 @@ import GridLayout from "react-grid-layout";
 import React, { useRef, useEffect, useState } from "react";
 import Video_Lidar from "./video/2D_Lidar_Video.mp4";
 import Video_Camera from "./video/Thermal_Camera_Video.mp4";
+
 import {
     Chart as ChartJS,
     RadialLinearScale,
@@ -10,11 +11,11 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
-import { PolarArea } from 'react-chartjs-2';
+import { PolarArea, Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
-export const data = {
+export const data1 = {
     labels: ['Safe', 'Confusion', 'Median for risk', 'Fatal'],
     datasets: [
         {
@@ -31,7 +32,63 @@ export const data = {
     ],
 };
 
+export const data2 = {
+    datasets: [
+        {
+            // label: 'MAX capacity',
+            data: [2, 4, 7,],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(240, 240, 240, 1.0)',
+                'rgba(240, 240, 240, 1.0)',
+            ],
+            borderWidth: 1,
+            borderColor: ['rgba(0,0,0,0)'],
+        },
+    ],
+};
+
+export const data3 = {
+    //labels: ['Safe', 'Confusion', 'Median for risk', 'Fatal'],
+    datasets: [
+        {
+            // label: 'MAX capacity',
+            data: [2, 4, 7,],
+            backgroundColor: [
+                'rgba(240, 240, 240, 1.0)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(240, 240, 240, 1.0)',
+            ],
+            borderWidth: 1,
+            borderColor: ['rgba(0,0,0,0)'],
+        },
+    ],
+};
+
+export const data4 = {
+    //labels: ['Safe', 'Confusion', 'Median for risk', 'Fatal'],
+    datasets: [
+        {
+            // label: 'MAX capacity',
+            data: [2, 4, 7],
+            backgroundColor: [
+                'rgba(240, 240, 240, 1.0)',
+                'rgba(240, 240, 240, 1.0)',
+                'rgba(255, 99, 132, 0.5)',
+            ],
+            borderWidth: 1,
+            borderColor: ['rgba(0,0,0,0)'],
+        },
+    ],
+};
+
 function MainPage() {
+    // useEffect(() => {
+    //     const{QWebEngineView, QUrl} = window.pyqtAPI;
+    //     const webview = new QWebEngineView();
+    //     webview.load(QUrl("http://localhost:3000"));
+    //     document.getElementById('pyqt-container').appendChild(webview);
+    // },[]);
     const videoRef1 = useRef();
     const videoRef2 = useRef();
     const [gridLayout, setGridLayout] = useState([
@@ -114,11 +171,15 @@ function MainPage() {
                 </div>
                 <div key="max-capacity">
                     <h2 className="chart_title">People Capacity per 1 square meter of space</h2>
-                    <PolarArea className="chart" data={data}/>
+                    <PolarArea className="chart" data={data1}/>
                 </div>
                 <div key="risk-level">
                     <h2 className="chart_title">Risk Analysis</h2>
-
+                    <div className="doughnut-chart">
+                        <Doughnut className="doughnut" data={data2}/>
+                        <Doughnut className="doughnut" data={data3}/>
+                        <Doughnut className="doughnut" data={data4}/>
+                    </div>
                 </div>
             </GridLayout>
         </div>
